5e36e1d2cd5ffd47e99d5edc2e733537fead486a
Chinmay Hundekari <chinmay.hundekari@gmail.com>
2014-04-11 23:59:38 -0400

    Commented unnecessary statements and added ctags

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 mkdir -v -p vim/bundle
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
-git submodule add https://github.com/tpope/vim-pathogen.git vim/bundle/vim-pathogen
-git submodule add https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
-git submodule add https://github.com/vim-scripts/taglist.vim.git vim/bundle/taglist.vim
+#git submodule add https://github.com/tpope/vim-pathogen.git vim/bundle/vim-pathogen
+#git submodule add https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
+#git submodule add https://github.com/vim-scripts/taglist.vim.git vim/bundle/taglist.vim
+#git submodule add https://github.com/vim-scripts/ctags.vim.git vim/bundle/ctags.vim
+
 
 
