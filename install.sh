dbdbab9c930e1f70babba7e2339c4e71b09eeff5
Chinmay <chinmay.hundekari@gmail.com>
2014-04-21 23:33:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-mkdir -v -p vim/bundle
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
+mkdir -v -p vim/bundle
+sudo mv ~/.vimrc ~/.vimrc_old
+sudo ln -s ~/dotfiles/vimrc ~/.vimrc
+sudo ln -s  ~/dotfiles/vim ~/.vim
+
 #git submodule add https://github.com/tpope/vim-pathogen.git vim/bundle/vim-pathogen
 #git submodule add https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
 #git submodule add https://github.com/vim-scripts/taglist.vim.git vim/bundle/taglist.vim
